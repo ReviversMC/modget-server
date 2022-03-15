8e2ea37a84d289e8928b63949b8035248ba3bb08
NebelNidas <burnerjulian@gmail.com>
2022-03-15 16:06:45 +0100

    Rename main class

--- src/main/java/com/github/reviversmc/modget/restservice/ModgetServer.java
+++ src/main/java/com/github/reviversmc/modget/restservice/ModgetServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ModgetManifestRestApiApplication {
+public class ModgetServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ModgetManifestRestApiApplication.class, args);
+		SpringApplication.run(ModgetServer.class, args);
 	}
 
 }
